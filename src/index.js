5b87f2dc19fd976db617380e528df5803c618262
Serhii Kondratiuk <sergey932015@gmail.com>
2021-09-27 18:21:34 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import './styles.css';
 import menuItemsTpl from './templates/menu-items.hbs';
 import menuItems from './menu.json';
-const menuRef = document.querySelector('.js-menu');
+
+const refs = {
+ menuRef: document.querySelector('.js-menu'),
+ checkBoxRef: document.querySelector('#theme-switch-toggle'),
+};
+
+const Theme = {
+ LIGHT: 'light-theme',
+ DARK: 'dark-theme',
+};
+
 const markup = menuItemsTpl(menuItems);
-menuRef.insertAdjacentHTML('beforeend', markup);
+refs.menuRef.insertAdjacentHTML('beforeend', markup);
+refs.checkBoxRef.addEventListener('change', onThemeSwitch);
+
+function toggleClass(addClass, revomeClass) {
+ document.body.classList.add(addClass);
+ document.body.classList.remove(revomeClass);
+}
+
+function onThemeSwitch(e) {
+ if (e.target.checked) {
+  toggleClass(Theme.DARK, Theme.LIGHT);
+  localStorage.setItem('Theme', Theme.DARK);
+ } else if (!e.target.checked) {
+  toggleClass(Theme.LIGHT, Theme.DARK);
+  localStorage.setItem('Theme', Theme.LIGHT);
+ }
+}
+
+function onPageReload() {
+ const getTheme = localStorage.getItem('Theme');
+ if (getTheme === Theme.DARK) {
+  refs.checkBoxRef.checked = 'true';
+  document.body.classList.add(Theme.DARK);
+ }
+}
+onPageReload();
